4dab67601638e7e02842603bbe0120a9e82ecdc2
rmcqueen <schroener1@gmail.com>
2017-10-14 16:33:13 -0700

    Include boilerplate for Redis.h and Redis.cpp

--- src/Redis.cpp
+++ src/Redis.cpp
@@ -1,5 +1,28 @@
-//
-// Created by Ryan on 2017-10-14.
-//
+#include <vector>
+#include <opencv2/opencv.hpp
+#include <regex>
+#include <time.h>
+#include <stdio.h>
 
 #include "Redis.h"
+#include "hiredis/hiredis.h"
+#include "PerceptualHash.h"
+
+
+using namespace std;
+
+void Redis::AddHashesToRedis(string image_path) {
+}
+
+void Redis::FindHashMatchInRedis(string image_name) {
+
+}
+
+void Redis::CompareTwoImages(string first_image, string second_image) {
+
+}
+
+void Redis::ClearRedis() {
+
+}
+
--- src/Redis.h
+++ src/Redis.h
@@ -1,11 +1,38 @@
-//
-// Created by Ryan on 2017-10-14.
-//
-
+#pragma once
 #ifndef REVERSE_IMAGE_SEARCH_REDIS_H
 #define REVERSE_IMAGE_SEARCH_REDIS_H
 
+#include <stdio.h>
+
+using namespace std;
+
 class Redis {
+
+ private:
+  const char *hostName;
+  int port_;
+
+  void AddHashesToRedis(string image_path);
+
+  void FindHashMatchInRedis(string image_name);
+
+  void CompareTwoImages(string first_image, string second_image);
+
+  void ClearRedis();
+
+ public:
+  Redis(char *host_name, int port) {
+    hostName = host_name;
+    port_ = port;
+  }
+
+  const int get_port() {
+    return port_;
+  };
+
+  const char &get_host_name() const {
+    return *hostName;
+  };
 
 };
 
