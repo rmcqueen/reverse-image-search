c8852d48f517215cc82dcd8e54780bda3d8d8552
rmcqueen <schroener1@gmail.com>
2017-10-25 00:16:17 -0700

    Clean up unused functionality, enhance code documentation

--- include/Image.h
+++ include/Image.h
@@ -1,8 +1,3 @@
-/*
- * Modified version of ShapeAndPositionInvariantImage.h
- * Source location: https://github.com/pippy360/transformationInvariantImageSearch/blob/master/fullEndToEndDemo/src/ShapeAndPositionInvariantImage.h
-*/
-
 #pragma once
 #ifndef REVERSE_IMAGE_SEARCH_IMAGE_H
 #define REVERSE_IMAGE_SEARCH_IMAGE_H
@@ -11,21 +6,20 @@
 #include <vector>
 #include <opencv2/opencv.hpp>
 
-#include "KeyPoint.h"
 
 using namespace std;
 
 class Image {
  public:
-  Image(string imageName,
-        cv::Mat imageData,
-        string imageFullPath);
+  Image(string &image_name,
+        cv::Mat &image_data,
+        string &image_full_path);
 
-  cv::Mat get_image_data() const {
+  cv::Mat get_image_data() {
     return image_data_;
   }
 
-  const string &get_full_image_path() const {
+  string get_full_image_path() {
     return image_full_path_;
   }
 
@@ -35,10 +29,12 @@ class Image {
   const string image_full_path_;
 };
 
-inline Image::Image(const string image_name,
-                    const cv::Mat image_data,
-                    const string image_full_path) : image_name_(image_name),
-                                                  image_data_(image_data),
-                                                   image_full_path_(image_full_path) {}
+inline Image::Image(
+    string &image_name,
+    cv::Mat &image_data,
+    string &image_full_path) :
+    image_name_(image_name),
+    image_data_(image_data),
+    image_full_path_(image_full_path) {}
 
 #endif //REVERSE_IMAGE_SEARCH_IMAGE_H
